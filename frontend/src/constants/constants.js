4e44403d8625c75de7ce9861ca81124c2ec6a30a
AGNES LILY <84066032+agneslily-2772@users.noreply.github.com>
2021-11-14 15:58:41 +0530

    File upload

---
+++ frontend/src/constants/constants.js
@@ -0,0 +1,7 @@
+export const CREATE = 'CREATE';
+export const UPDATE = 'UPDATE';
+export const DELETE = 'DELETE';
+export const FETCH_ALL = 'FETCH_ALL';
+export const LIKE = 'LIKE';
+export const PRODUCTION_URL = "http://localhost:2772";
+export const DEVELOPMENT_URL = "https://blogybook.herokuapp.com";
